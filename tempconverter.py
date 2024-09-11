9b78b926ba5d4a46362dd6f36666337280a82dfe
valeriedalsa <valeriedalsaso@gmail.com>
2024-09-11 16:41:05 -0400

    Delete tempconverter.py

--- tempconverter.py
+++
@@ -1,7 +0,0 @@
-temp_celsius = int(input("Print the tempurature in Celsius: "))
-fahrenheit = temp_celsius * 9/5 + 32
-print('This is the tempurature converted to fahrenheit =',fahrenheit)
-
-
-
-      
