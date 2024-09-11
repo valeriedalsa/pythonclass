957aa2573a802c477649275ece59d2983ee32e3e
valeriedalsa <valeriedalsaso@gmail.com>
2024-09-11 16:58:32 -0400

    Update tempconverter.py

--- tempconverter.py
+++ tempconverter.py
@@ -1,4 +1,4 @@
-temp_celsius = int(input("Print the tempurature in Celsius: "))
+temp_celsius = float(input("Print the tempurature in Celsius: "))
 Fahrenheit = temp_celsius * 9/5 + 32
 print('This is the tempurature converted to Fahrenheit =',Fahrenheit)
 
