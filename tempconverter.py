0fbf03ba46fda61af0ee5042795c9d7fbeddbeed
valeriedalsa <valeriedalsaso@gmail.com>
2024-09-11 16:42:04 -0400

    Add files via upload
    Code for Celsius to Fahrenheit calculator.

---
+++ tempconverter.py
@@ -0,0 +1,7 @@
+temp_celsius = int(input("Print the tempurature in Celsius: "))
+Fahrenheit = temp_celsius * 9/5 + 32
+print('This is the tempurature converted to Fahrenheit =',Fahrenheit)
+
+
+
+      
