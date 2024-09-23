d06a1ef3fdfcf14973cfef83cfd2a94621278683
valeriedalsa <valeriedalsaso@gmail.com>
2024-09-23 17:11:10 -0400

    Update state+countytaxes.py

--- state+countytaxes.py
+++ state+countytaxes.py
@@ -3,8 +3,8 @@ def main():
     county_sales_tax = original * 0.025
     state_sales_tax = original * 0.05
     total = state_sales_tax + county_sales_tax + original
-    print(f'''Your purchase was ${original:.2f}, your state sales tax for this purchase is ${state_sales_tax:.2f},
-your county tax is ${county_sales_tax:.2f}, and your total is ${total:.2f}''')
+    print(f'''Your purchase was ${original:,.2f}, your state sales tax for this purchase is ${state_sales_tax:,.2f},
+your county tax is ${county_sales_tax:.2f}, and your total is ${total:,.2f}''')
 main()
 
                     
