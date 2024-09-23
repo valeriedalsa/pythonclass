b4aa8e1b37f34883dc185c1b1e1f9063c4840ba3
valeriedalsa <valeriedalsaso@gmail.com>
2024-09-23 17:08:50 -0400

    Update state+countytaxes.py

--- state+countytaxes.py
+++ state+countytaxes.py
@@ -1,6 +1,6 @@
 def main():
     original = (float(input("How much was your purchase? ")))
-    county_sales_tax = original * 2.5
+    county_sales_tax = original * 0.025
     state_sales_tax = original * 0.05
     total = state_sales_tax + county_sales_tax + original
     print(f'''Your purchase was ${original:.2f}, your state sales tax for this purchase is ${state_sales_tax:.2f},
